4ec567bd962c2beb3a129b9fb1fc13de342b2463
Ankai Jie <ankaijie@gmail.com>
2016-04-24 21:42:59 -0400

    user interface completed

--- csvWriter.py
+++ csvWriter.py
@@ -14,17 +14,15 @@ NOTE: This CSV Writer was written for one specific author. It is hard-coded data
 
 import csv
 
-'''
-totalJournalDict = {' Wireless Networks': 67, ' Wireless networks': 18, ' Sensors': 13, ' … ': 11, ' Selected Areas in  …': 8, ' Wireless  …': 8, ' Multimedia Tools and Applications': 7, ' EURASIP Journal on  …': 6, ' EURASIP Journal on Wireless  …': 6, ' …  Surveys & Tutorials': 5, 'Wattar ': 4, ' EURASIP Journal on Wireless …': 4, ' Internet of Things  …': 3, ' Wireless Personal Communications': 3, ' Nano Communication Networks': 3, ' Signal Processing': 3, ' Vehicular Technology': 3, ' EURASIP Journal on Wireless Communications and  …': 3, ' Mobile Computing': 3, ' NanoBioscience': 2, ' Access': 2, ' Vehicular  …': 2, ' Systems Journal': 2, ' Communications  …': 2, ' 2015 ': 2, ' Sensors Journal': 2, ' Proceedings of the 8th  …': 2, ' Journal of Sensor and Actuator Networks': 2, ' Parallel and Distributed  …': 2, ' Mobile Networks and  …': 2, ' 计算机科学': 2, ' arXiv preprint arXiv: …': 2, ' …  in Biomedicine': 2, ' …  (GLOBECOM)': 2, 'Mougy': 2, ' Intelligent Transportation Systems': 2, ' Communications (ICC)': 2, ' INFOCOM': 2, ' Internet and Distributed  …': 2, ' International Journal of Distributed  …': 2, ' Proceedings of ACM The First Annual International  …': 2, ' Nanotechnology': 2, ' Eurasip journal on wireless communications …': 2, ' Journal of AI and Data  …': 2, ' Journal of Network and  …': 2, ' The Journal of Supercomputing': 1, ' The Scientific World  …': 1, ' Online Conference on …': 1, ' Proceedings of the  …': 1, ' Proceedings of the 2nd  …': 1, ' …  and Networks (ICCCN)': 1, 'Ibáñez': 1, ' …  and Networking Conference (WCNC)': 1, ' Wireless Networks ': 1, ' Global Information  …': 1, ' …  (MeMeA)': 1, ' …  Conference Fall (VTC  …': 1, ' Advances in Computing …': 1, ' Selected Areas in Communications': 1, ' …  (Cloudnet)': 1, ' International Journal of  …': 1, ' Network of the Future (NOF …': 1, ' Web Information Systems and Mining (WISM)': 1, ' Personal Indoor and Mobile  …': 1, ' Journal of Sensors': 1, ' Proceedings of the seventh ACM international  …': 1, ' High Performance Computing  …': 1, ' Green Technologies Conference …': 1, ' arXiv preprint arXiv:1102.2608': 1, ' Bio': 1, ' Future Generation Computer  …': 1, ' IEEE Transactions on  …': 1, ' arXiv preprint arXiv:1406.1867': 1, ' arXiv preprint arXiv:1408.2078': 1, ' Cloud Computing (CLOUD)': 1, ' Journal of Ambient Intelligence and Humanized  …': 1, ' IEEE Communications  …': 1, ' AAAI': 1, ' …  Sensor Systems': 1, ' Cloud Computing Technology …': 1, ' Parallel and Distributed Systems': 1, ' Computer Communications': 1, ' Mobile Networks and Applications': 1, ' Economics of Grids': 1, ' 电子学报': 1, ' ieeexplore.ieee.org': 1, ' INTERNATIONAL JOURNAL ON SMART  …': 1, ' Control': 1, ' Information Systems  …': 1, ' Multisensor Fusion and  …': 1, ' …  Systems (ICDCS)': 1, ' Journal of Computers': 1, ' Cloud Computing': 1, ' Pervasive Computing (ICPC)': 1, ' 软件学报': 1, ' 2011 ': 1, ' Network Protocols (ICNP)': 1, ' J Converg': 1, ' Computer Software and  …': 1, ' …  (BlackSeaCom)': 1, ' …  on Mobile Ad Hoc Networking and  …': 1, ' …  (VTC Spring)': 1, ' Network': 1, ' power': 1, ' Simulation Modelling Practice and Theory': 1, 'Saavedra': 1, ' Intelligent Transportation …': 1, ' Intelligent  …': 1, ' …  Workshops (ICC)': 1, ' Computer Science and  …': 1, ' Advance Computing Conference (IACC)': 1, ' KSII Transactions on Internet &  …': 1, ' International Journal of Control': 1, ' Artificial Intelligence Review': 1, ' Telecommunication Systems': 1, ' Proceedings of the 19th …': 1, ' Computational Problem': 1, ' TELKOMNIKA Indonesian Journal of …': 1, ' Computers & Electrical  …': 1, ' EURASIP Journal on Wireless Communications …': 1, ' Nano Communication  …': 1, ' Journal of Internet  …': 1, ' Green Communications and  …': 1, ' Intelligent Sensors': 1, ' 2013 IEEE 24th Annual International  …': 1, ' Journal of Network and Systems  …': 1, ' Pervasive Computing and  …': 1, ' e': 1, ' Computer Networks': 1, ' …  Conference (WCNC)': 1, ' Modeling & Optimization …': 1, ' 2010': 1, ' 计算机应用研究': 1, ' Innovative Mobile and Internet Services in  …': 1, ' Communications': 1, ' …  Assurance and Security (IAS)': 1, ' …  Processing and their …': 1, ' Journal of the American  …': 1, ' Wireless Days (WD)': 1, 'Valenzuela': 1, ' Journal of medical systems': 1, ' Information Theory and Applications  …': 1, ' Green Communications  …': 1, 'Rivera': 1, ' Communications Letters': 1, ' Sensor': 1, ' Signals': 1, ' Ad Hoc Networks': 1, ' Transactions on  …': 1, ' The Journal of  …': 1, ' ACM Transactions on  …': 1, ' Journal of Network and Computer Applications': 1, 'Wei': 1, ' Internet of Things Journal': 1, ' KTH Royal Institute of Technology': 1, ' International Journal of Antennas and Propagation': 1, ' Online Conference on Green Communications ( …': 1, ' Real': 1, ' EURASIP Journal on Wireless Communications  …': 1, ' Web Services (ICWS …': 1, ' International Journal On  …': 1, ' Communication Technologies for Vehicles': 1, ' Routing in Opportunistic Networks': 1, ' Wireless and Mobile  …': 1, ' …  Areas in Communications': 1, ' Cybernetics': 1, ' …  Networks and Self': 1, ' Computer  …': 1, ' BioNanoScience': 1, ' PhD dissertations': 1, ' …  Conference (VTC Spring)': 1, ' Multimedia tools and  …': 1, ' …  (CLOUD)': 1, ' Proceedings of the World Congress on  …': 1}
 
-
-writer = csv.writer(open('dict.csv', 'w'))
-for key, value in totalJournalDict.items():
-    try:
-        writer.writerow([key, value])
-    except UnicodeEncodeError:
-        continue 
-'''
+def total_journal_dict_writer (totalJournalDict,name):
+#totalJournalDict = {' Wireless Networks': 67, ' Wireless networks': 18, ' Sensors': 13, ' … ': 11, ' Selected Areas in  …': 8, ' Wireless  …': 8, ' Multimedia Tools and Applications': 7, ' EURASIP Journal on  …': 6, ' EURASIP Journal on Wireless  …': 6, ' …  Surveys & Tutorials': 5, 'Wattar ': 4, ' EURASIP Journal on Wireless …': 4, ' Internet of Things  …': 3, ' Wireless Personal Communications': 3, ' Nano Communication Networks': 3, ' Signal Processing': 3, ' Vehicular Technology': 3, ' EURASIP Journal on Wireless Communications and  …': 3, ' Mobile Computing': 3, ' NanoBioscience': 2, ' Access': 2, ' Vehicular  …': 2, ' Systems Journal': 2, ' Communications  …': 2, ' 2015 ': 2, ' Sensors Journal': 2, ' Proceedings of the 8th  …': 2, ' Journal of Sensor and Actuator Networks': 2, ' Parallel and Distributed  …': 2, ' Mobile Networks and  …': 2, ' 计算机科学': 2, ' arXiv preprint arXiv: …': 2, ' …  in Biomedicine': 2, ' …  (GLOBECOM)': 2, 'Mougy': 2, ' Intelligent Transportation Systems': 2, ' Communications (ICC)': 2, ' INFOCOM': 2, ' Internet and Distributed  …': 2, ' International Journal of Distributed  …': 2, ' Proceedings of ACM The First Annual International  …': 2, ' Nanotechnology': 2, ' Eurasip journal on wireless communications …': 2, ' Journal of AI and Data  …': 2, ' Journal of Network and  …': 2, ' The Journal of Supercomputing': 1, ' The Scientific World  …': 1, ' Online Conference on …': 1, ' Proceedings of the  …': 1, ' Proceedings of the 2nd  …': 1, ' …  and Networks (ICCCN)': 1, 'Ibáñez': 1, ' …  and Networking Conference (WCNC)': 1, ' Wireless Networks ': 1, ' Global Information  …': 1, ' …  (MeMeA)': 1, ' …  Conference Fall (VTC  …': 1, ' Advances in Computing …': 1, ' Selected Areas in Communications': 1, ' …  (Cloudnet)': 1, ' International Journal of  …': 1, ' Network of the Future (NOF …': 1, ' Web Information Systems and Mining (WISM)': 1, ' Personal Indoor and Mobile  …': 1, ' Journal of Sensors': 1, ' Proceedings of the seventh ACM international  …': 1, ' High Performance Computing  …': 1, ' Green Technologies Conference …': 1, ' arXiv preprint arXiv:1102.2608': 1, ' Bio': 1, ' Future Generation Computer  …': 1, ' IEEE Transactions on  …': 1, ' arXiv preprint arXiv:1406.1867': 1, ' arXiv preprint arXiv:1408.2078': 1, ' Cloud Computing (CLOUD)': 1, ' Journal of Ambient Intelligence and Humanized  …': 1, ' IEEE Communications  …': 1, ' AAAI': 1, ' …  Sensor Systems': 1, ' Cloud Computing Technology …': 1, ' Parallel and Distributed Systems': 1, ' Computer Communications': 1, ' Mobile Networks and Applications': 1, ' Economics of Grids': 1, ' 电子学报': 1, ' ieeexplore.ieee.org': 1, ' INTERNATIONAL JOURNAL ON SMART  …': 1, ' Control': 1, ' Information Systems  …': 1, ' Multisensor Fusion and  …': 1, ' …  Systems (ICDCS)': 1, ' Journal of Computers': 1, ' Cloud Computing': 1, ' Pervasive Computing (ICPC)': 1, ' 软件学报': 1, ' 2011 ': 1, ' Network Protocols (ICNP)': 1, ' J Converg': 1, ' Computer Software and  …': 1, ' …  (BlackSeaCom)': 1, ' …  on Mobile Ad Hoc Networking and  …': 1, ' …  (VTC Spring)': 1, ' Network': 1, ' power': 1, ' Simulation Modelling Practice and Theory': 1, 'Saavedra': 1, ' Intelligent Transportation …': 1, ' Intelligent  …': 1, ' …  Workshops (ICC)': 1, ' Computer Science and  …': 1, ' Advance Computing Conference (IACC)': 1, ' KSII Transactions on Internet &  …': 1, ' International Journal of Control': 1, ' Artificial Intelligence Review': 1, ' Telecommunication Systems': 1, ' Proceedings of the 19th …': 1, ' Computational Problem': 1, ' TELKOMNIKA Indonesian Journal of …': 1, ' Computers & Electrical  …': 1, ' EURASIP Journal on Wireless Communications …': 1, ' Nano Communication  …': 1, ' Journal of Internet  …': 1, ' Green Communications and  …': 1, ' Intelligent Sensors': 1, ' 2013 IEEE 24th Annual International  …': 1, ' Journal of Network and Systems  …': 1, ' Pervasive Computing and  …': 1, ' e': 1, ' Computer Networks': 1, ' …  Conference (WCNC)': 1, ' Modeling & Optimization …': 1, ' 2010': 1, ' 计算机应用研究': 1, ' Innovative Mobile and Internet Services in  …': 1, ' Communications': 1, ' …  Assurance and Security (IAS)': 1, ' …  Processing and their …': 1, ' Journal of the American  …': 1, ' Wireless Days (WD)': 1, 'Valenzuela': 1, ' Journal of medical systems': 1, ' Information Theory and Applications  …': 1, ' Green Communications  …': 1, 'Rivera': 1, ' Communications Letters': 1, ' Sensor': 1, ' Signals': 1, ' Ad Hoc Networks': 1, ' Transactions on  …': 1, ' The Journal of  …': 1, ' ACM Transactions on  …': 1, ' Journal of Network and Computer Applications': 1, 'Wei': 1, ' Internet of Things Journal': 1, ' KTH Royal Institute of Technology': 1, ' International Journal of Antennas and Propagation': 1, ' Online Conference on Green Communications ( …': 1, ' Real': 1, ' EURASIP Journal on Wireless Communications  …': 1, ' Web Services (ICWS …': 1, ' International Journal On  …': 1, ' Communication Technologies for Vehicles': 1, ' Routing in Opportunistic Networks': 1, ' Wireless and Mobile  …': 1, ' …  Areas in Communications': 1, ' Cybernetics': 1, ' …  Networks and Self': 1, ' Computer  …': 1, ' BioNanoScience': 1, ' PhD dissertations': 1, ' …  Conference (VTC Spring)': 1, ' Multimedia tools and  …': 1, ' …  (CLOUD)': 1, ' Proceedings of the World Congress on  …': 1}
+    writer = csv.writer(open(name+'.csv', 'w'))
+    for key, value in totalJournalDict.items():
+        try:
+            writer.writerow([key, value])
+        except UnicodeEncodeError:
+            continue
 
 '''
 indJournalArrays = [['Body area networks: A survey', {' …  Processing and their …': 1, ' Information Theory and Applications  …': 1, ' Journal of Network and  …': 1, ' Selected Areas in Communications': 1, ' Parallel and Distributed Systems': 1, ' Network': 1, ' …  in Biomedicine': 2, ' Multimedia Tools and Applications': 1, ' Journal of medical systems': 1, ' …  Assurance and Security (IAS)': 1, ' …  Surveys & Tutorials': 2, ' Journal of the American  …': 1, ' Computer Science and  …': 1, ' Cloud Computing Technology …': 1, ' Future Generation Computer  …': 1, ' Signal Processing': 1, ' Wireless  …': 1, ' Sensors': 4, ' Mobile Networks and  …': 1, ' EURASIP Journal on Wireless Communications …': 1, ' International Journal of Distributed  …': 1, 'Valenzuela': 1, ' The Journal of  …': 1, ' Systems Journal': 1, ' Multimedia tools and  …': 1}],
@@ -51,16 +49,18 @@ indJournalArrays = [['Body area networks: A survey', {' …  Processing and thei
 
 ['A survey on the ietf protocol suite for the internet of things: Standards, challenges, and opportunities', {' Wireless Networks': 10, 'Wattar ': 1, ' Proceedings of the  …': 1, ' ACM Transactions on  …': 1, ' Wireless networks': 1, ' International Journal On  …': 1, ' Journal of Ambient Intelligence and Humanized  …': 1, ' Intelligent Sensors': 1, ' Sensors': 1, ' Web Services (ICWS …': 1, ' … (ICET)': 1, ' Wireless and Mobile  …': 1, ' Internet of Things Journal': 1, ' Communications  …': 1, ' Access': 1, ' Journal of Network and Computer Applications': 1, ' Multimedia Tools and Applications': 1, ' 软件学报': 1, ' Network of the Future (NOF …': 1, ' EURASIP Journal on Wireless …': 1, ' Internet of Things  …': 1}]
 ]
-
-writer = csv.writer(open('inidividualJournals.csv', 'w'))
-for paper in indJournalArrays:
-    writer.writerow([paper[0]])
-    for key, value in paper[1].items():
-        try:
-            writer.writerow([key, value])
-        except UnicodeEncodeError:
-            continue 
 '''
+
+def jounal_dict_writer(indJournalArrays, name):
+    writer = csv.writer(open(name + '.csv', 'w'), lineterminator='\n')
+    for paper in indJournalArrays:
+        writer.writerow([paper[0]])
+        for key, value in paper[1].items():
+            try:
+                writer.writerow([key, value])
+            except UnicodeEncodeError:
+                continue
+        writer.writerow(['\n'])
 
 
 '''
@@ -76,37 +76,39 @@ self_cite_arr = [{'Paper Title': 'Body area networks: A survey', 'Self Cites': 1
 {'Paper Title': 'EDAL: An energy-efficient, delay-aware, and lifetime-balancing data collection protocol for heterogeneous wireless sensor networks', 'Self Cites': 1},
 {'Paper Title': 'A survey on topology control in wireless sensor networks: Taxonomy, comparative study, and open issues', 'Self Cites': 1},
 {'Paper Title': 'A survey on the ietf protocol suite for the internet of things: Standards, challenges, and opportunities', 'Self Cites': 'No Valid PDF URL in GSC'}]
-
-keys = self_cite_arr[0].keys()
-with open('self_cites.csv', 'w') as output_file:
-    dict_writer = csv.DictWriter(output_file, keys)
-    dict_writer.writeheader()
-    dict_writer.writerows(self_cite_arr)
 '''
 
+def self_cite_writer(self_cite_arr, name):
+    keys = self_cite_arr[0].keys()
+    with open(name + '.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(self_cite_arr)
 
 
+
+'''
 over_cite_arr = [[{'Over-cite Count': 28, 'Citing Paper Number': 1}, 'Paper Title: Spatial reusability-aware routing in multi-hop wireless networks'],
  [{'Over-cite Count': 7, 'Citing Paper Number': 1}, {'Over-cite Count': 4, 'Citing Paper Number': 2}, 'Paper Title: CDC: Compressive data collection for wireless sensor networks'],
  [{'Over-cite Count': 13, 'Citing Paper Number': 1}, {'Over-cite Count': 0, 'Citing Paper Number': 2}, {'Over-cite Count': 8, 'Citing Paper Number': 3}, 'Paper Title: EDAL: An energy-efficient, delay-aware, and lifetime-balancing data collection protocol for heterogeneous wireless sensor networks'],
  [{'Over-cite Count': 1, 'Citing Paper Number': 1}, 'Paper Title: Interference-based topology control algorithm for delay-constrained mobile ad hoc networks'],
  [{'Over-cite Count': 0, 'Citing Paper Number': 1}, {'Over-cite Count': 1, 'Citing Paper Number': 2}, 'Paper Title: Physarum optimization: A biology-inspired algorithm for the steiner tree problem in networks']]
+'''
 
+def over_cite_writer(over_cite_arr, name):
 
-writer = csv.writer(open('over_cites2.csv', 'w'), lineterminator='\n')
-for paper in over_cite_arr:
-    
-    writer.writerow([paper[-1]])
-    arr_len = len(paper)
-    headers =['Citing Paper Number', 'Over-cite Count']
-    writer.writerow(headers)
-    paper.pop()
-    total =0
-    for dict_item in paper:
-        writer.writerow([dict_item['Citing Paper Number'], dict_item['Over-cite Count']])
-        total+=dict_item['Over-cite Count']
-    writer.writerow(['Total', total])
-    writer.writerow(['\n'])
+    writer = csv.writer(open(name + '.csv', 'w'), lineterminator='\n')
+    for paper in over_cite_arr:
 
-    
+        writer.writerow([paper[-1]])
+        arr_len = len(paper)
+        headers =['Citing Paper Number', 'Over-cite Count']
+        writer.writerow(headers)
+        paper.pop()
+        total =0
+        for dict_item in paper:
+            writer.writerow([dict_item['Citing Paper Number'], dict_item['Over-cite Count']])
+            total+=dict_item['Over-cite Count']
+        writer.writerow(['Total', total])
+        writer.writerow(['\n'])
 
--- scrapper.py
+++ scrapper.py
@@ -16,6 +16,7 @@ import time
 import PyPDF2
 from _csv import Error
 import traceback
+from csvWriter import *
 
 #these two functions take and authors first name and last name, and convert them to how it would appear in the references
 # section of a paper from the specified publisher
@@ -78,36 +79,44 @@ def get_ref_author_format(fname, lname, pub):
     
 
 # given a paper, counts the number of times it cites an author of the paper
-def count_self_cites(author, paper):
-    paper_authors = paper.getInfo()['Authors'].lower()
-    fname = author.getFirstName()
-    lname = author.getLastName()
-    
-    '''fname = 'athanasios'
-    lname = 'vasilakos'''
-    
-    
-    if (paper_authors.find(fname) == -1 or paper_authors.find(lname) == -1):
-        print('Error: author: ' + fname + ' ' + lname + ' is not a valid author of this paper.')
-        return -1
-    
-    auth_word = get_ref_author_format(fname, lname, paper.getInfo()['Publisher'])
-    
-    analyzer = PaperReferenceExtractor()
-    
-    pdfUrl = paper.getPdfUrl()
-    refContent = analyzer.getReferencesContent(pdfUrl)
-    
-    if (pdfUrl is not None and refContent is not None):
-        
-        numCites = analyzer.getCitesToAuthor(auth_word, refContent)
-        #print (fname+ ' '+lname+ ' has '+str(numCites)+' number of self-cites in paper: '+ paper.getInfo()['Title'])
-        self_cites_info = {'Paper Title': paper.getInfo()['Title'], 'Self Cites': numCites}
-        return self_cites_info
-    else:
-        self_cites_info = {'Paper Title': paper.getInfo()['Title'], 'Self Cites': 'No Valid PDF URL in GSC'}
-        return self_cites_info
+def count_self_cites(author, num_load):
+    author.loadPapers(num_load)
+    self_cite_arr = []
+    for paper in author.getPapers():
+        paper_authors = paper.getInfo()['Authors'].lower()
+        fname = author.getFirstName()
+        lname = author.getLastName()
 
+        '''fname = 'athanasios'
+        lname = 'vasilakos'''
+
+
+        if (paper_authors.find(fname) == -1 or paper_authors.find(lname) == -1):
+            print('Error: author: ' + fname + ' ' + lname + ' is not a valid author of this paper.')
+            return -1
+
+        auth_word = get_ref_author_format(fname, lname, paper.getInfo()['Publisher'])
+
+        analyzer = PaperReferenceExtractor()
+
+        pdfUrl = paper.getPdfUrl()
+        if (pdfUrl is None):
+            print('No PDF url for this paper, skipping.')
+            continue
+        refContent = analyzer.getReferencesContent(pdfUrl)
+
+        if (refContent is not None):
+
+            numCites = analyzer.getCitesToAuthor(auth_word, refContent)
+            #print (fname+ ' '+lname+ ' has '+str(numCites)+' number of self-cites in paper: '+ paper.getInfo()['Title'])
+            self_cites_info = {'Paper Title': paper.getInfo()['Title'], 'Self Cites': numCites}
+        else:
+            self_cites_info = {'Paper Title': paper.getInfo()['Title'], 'Self Cites': 'No Valid PDF URL in GSC'}
+
+        print(self_cites_info)
+        self_cite_arr.append(self_cites_info)
+
+    return self_cite_arr
 
 #given an author, and a number of papers, returns the journal frequency list of the first 30 citing papers
 def count_journal_frequency (author, num_papers):
@@ -297,6 +306,78 @@ def count_cross_cites (author, x_most_rel, top_x):
     
     return final_info_dict
 
+
+
+print('Academic Fraud Detector')
+authorlink = input('Please enter the Google Scholar profile link of the author you want to investigate:\n')
+entered_author = AcademicPublisher(authorlink,1)
+
+while 1:
+    function_select = input('Please select the function you want to run on this author. Enter 1 for self-cites, 2 for over-cites,\n'
+                            '3 for cross-cites, and 4 for journal frequency. Enter anything else to exit program.\n')
+    if(function_select=='1'):
+        pap_num = int(input('Self-cites selected. Enter the number of papers you want to load:\n'))
+        csv_file_name = input('Do you want to output your result into a csv file?\nIf yes, enter the name of the file. If no, enter "n".\n')
+        entered_author.loadPapers(pap_num)
+        self_cite_arr = count_self_cites(entered_author, pap_num)
+        print(self_cite_arr)
+        if (csv_file_name is not 'n'):
+            self_cite_writer(self_cite_arr, csv_file_name)
+
+    elif (function_select=='2'):
+        pap_num = int(input('Over-cites selected. Enter the number of papers you want to load:\n'))
+        csv_file_name = input('Do you want to output your result into a csv file?\nIf yes, enter the name of the file. If no, enter "n".\n')
+
+        entered_author.loadPapers(pap_num)
+        over_cite_arr = []
+        try:
+
+            for paper in entered_author.getPapers():
+                arr = count_overcites(paper, entered_author)
+                k = "Paper Title: " + paper.getInfo()['Title']
+                arr.append(k)
+                over_cite_arr.append(arr)
+                print(arr)
+            print(over_cite_arr)
+        except AttributeError as e:
+            print('google scholar has blocked you.')
+            print(e)
+
+        if (csv_file_name is not 'n'):
+            over_cite_writer(over_cite_arr, csv_file_name)
+
+    elif (function_select=='3'):
+        x_most_rel = int(input('Cross-cites selected. Enter the number of papers you want to load for the authors:\n'))
+        auth_num = int(input('Enter the number of cited authors you want to analyze:\n'))
+        try:
+            print(count_cross_cites(entered_author, x_most_rel, auth_num))
+            print('FINAL RESULT')
+        except Error as e:
+            print(e)
+        except Exception as e:
+            print(e)
+        except AttributeError as e:
+            print(e)
+            traceback.print_exc()
+
+    elif (function_select == '4'):
+        pap_num = int(input('Journal frequency selected. Enter the number of papers you want to load:\n'))
+        csv_file_name = input('Do you want to output your result into a csv file?\nIf yes, enter the name of the file. If no, enter "n".\n')
+        journal_dict_arr = []
+        try:
+            journal_dict_arr = count_journal_frequency(entered_author, pap_num)
+            print(journal_dict_arr)
+            print('Journal Dict array')
+        except AttributeError as e:
+            print(e)
+            traceback.print_exc()
+
+        if (csv_file_name is not 'n'):
+            jounal_dict_writer(journal_dict_arr, csv_file_name)
+
+    else:
+        break
+
 '''
 #Cross_cites script for top 10 papers for Vasilakos 
 try:
@@ -345,7 +426,7 @@ except AttributeError as e:
 '''
 
 #getting more recent papers from vasilakos over cite data
-
+'''
 try:
     vas = AcademicPublisher('https://scholar.google.ca/citations?hl=en&user=_yWPQWoAAAAJ&view_op=list_works&sortby=pubdate', 80)
     time.sleep(10)
@@ -358,4 +439,4 @@ try:
 except AttributeError as e:
     print('google scholar has blocked you.')
     print(e)
-
+'''
