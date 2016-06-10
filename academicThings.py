a7cb892df0182afe993af134dc1f761a0cf12aa1
Ankai Jie <ankaijie@gmail.com>
2016-06-10 00:20:44 -0400

    off campus access set up

--- academicThings.py
+++ academicThings.py
@@ -8,6 +8,7 @@ import requests
 import lxml
 import re
 from ReferenceParser import IeeeReferenceParser, SpringerReferenceParser, PaperReferenceExtractor
+import offCampusLogin
 
 
 class Paper:
@@ -20,12 +21,14 @@ class Paper:
         self.__citedByNum = 0
         self.__allAuthors = None
 
+        #Internet Session Setup
+        self.session = offCampusLogin.getSesh()
+        self.headers = offCampusLogin.getHeaders()
+
         self.loadFromGoogleScholar()
 
     def loadFromGoogleScholar(self):
-        session = requests.session()
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}  
-        response = session.get(self.__url, headers=headers)
+        response = self.session.get(self.__url, headers=self.headers)
         soup = BeautifulSoup(response.content, 'lxml')
         #print(soup)
 
@@ -147,15 +150,19 @@ class AcademicPublisher:
         self.url = None
         self.__paper_list = []
 
+        #Internet Session Setup
+        self.session = offCampusLogin.getSesh()
+        self.headers = offCampusLogin.getHeaders()
+
         if (mainUrl is not None):
             self.url = mainUrl
             self.loadPapers(numPapers)
 
+
     def loadPapers(self, numPapers):
-        session = requests.Session()
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}  
-        response = session.get(self.url + '&cstart=0&pagesize=' + str(numPapers), headers=headers)
+        response = self.session.get(self.url + '&cstart=0&pagesize=' + str(numPapers), headers=self.headers)
         soup = BeautifulSoup(response.content, "lxml")
+
 
         full_name = soup.find('div', attrs={'id': 'gsc_prf_in'}).text.lower().split()
 
@@ -171,7 +178,7 @@ class AcademicPublisher:
         #appends all papers to paperlist
         for one_url in soup.findAll('a', attrs={'class': 'gsc_a_at'}, href=True):
             #one_url['href'] finds the link to the paper page
-            p = Paper('https://scholar.google.ca' + one_url['href'])
+            p = Paper('https://scholar-google-ca.proxy.lib.uwaterloo.ca' + one_url['href'])
             self.__paper_list.append(p)
             # takes out all papers not from IEEE or Springer US 
             self.filterByPublishers()
@@ -205,52 +212,76 @@ class AcademicPublisher:
 
 class GscPdfExtractor:
 
+    def __init__(self):
+        #Internet Session Setup
+        self.session = offCampusLogin.getSesh()
+        self.headers = offCampusLogin.getHeaders()
+
     #returns the list of pdf urls from the first page of citations on Google Scholar
     def findPapersFromCitations(self, citationsUrl):
-        session = requests.session()
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}  
-        response = session.get(citationsUrl, headers=headers)
+        response = self.session.get(citationsUrl, headers=self.headers)
         soup = BeautifulSoup(response.content, 'lxml')
 
         linkExtracts = soup.findAll('div', attrs={'class': 'gs_md_wp gs_ttss'})
+        if linkExtracts is None:
+            return None
         pdfUrls = []
 
         for extract in linkExtracts:
             #this code will skip links with [HTML] tag and throw error for links that are only "Get it at UWaterloo"
-            try:
-                if extract.find('span', attrs={'class': 'gs_ctg2'}).text == "[PDF]":
-                    pdfUrls.append(extract.find('a')['href'])
-                else:
-                    print(extract.find('span', attrs={'class': 'gs_ctg2'}).text + " tag process will be coded later")
-            except:
-                print('No tag, "Get it at waterloo" part.. to be coded later')
+            tag = extract.find('span', attrs={'class': 'gs_ctg2'})
+            if tag is not None and tag.text == "[PDF]":
+                print('pdf url')
+                pdfUrls.append(extract.find('a')['href'])
+            elif tag is not None:
+                print('Non-PDF tag, using get it @ waterloo')
 
+            potential_links = extract.findAll('a')
+            for link in potential_links:
+                if link.text.strip() == "Get It!@Waterloo":
+                    pdfUrls.append(self.getWatPDF('https://scholar-google-ca.proxy.lib.uwaterloo.ca'+link['href']))
         return pdfUrls
+
 
     #getting PDF url from paper info page, different from citation list page
     def findPdfUrlFromInfo(self, infoPageUrl):
-
-        session = requests.session()
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}  
-        response = session.get(infoPageUrl, headers=headers)
+        response = self.session.get(infoPageUrl, headers=self.headers)
         soup = BeautifulSoup(response.content, 'lxml')
 
-        linkExtracts = soup.findAll('div', attrs={'class': 'gsc_title_ggi'})
+        extract = soup.find('div', attrs={'id': 'gsc_title_gg'})
+        if extract is None:
+            return None
 
-        for extract in linkExtracts:
-            #this code will skip links with [HTML] tag and throw error for links that are only "Get it at UWaterloo"
-            try:
-                if extract.find('span', attrs={'class': 'gsc_title_ggt'}).text == "[PDF]":
-                    return extract.find('a')['href']
-                else:
-                    print("html tag, will figure out later")
-                    return None
-            except:
-                print("get it at waterloo link, will figure out later")
-                return None
+        #find pdf url
+        tag = extract.find('span', attrs={'class': 'gsc_title_ggt'})
+        if tag is not None and tag.text == "[PDF]":
+            print('pdf url')
+            return extract.find('a')['href']
+        elif tag is not None:
+            print('Non-PDF tag, using get it @ waterloo')
+
+        potential_links = extract.findAll('div', attrs={'class': 'gsc_title_ggi'})
+        for div in potential_links:
+            text =  div.text.strip()
+            if text == 'Get It!@Waterloo':
+                return self.getWatPDF(div.find('a')['href'])
+        return None
+
+
+    # Parses page waterloo gives us to extract pdf link of paper
+    def getWatPDF(self, url):
+        print(url)
+        return 'lol'
 
 
 class GscHtmlFunctions:
+
+    def __init__(self):
+        #Internet Session Setup
+        self.session = offCampusLogin.getSesh()
+        self.headers = offCampusLogin.getHeaders()
+
+
     def get_author_from_search(self, auth_name, paper_name):
         # taking out any special characters in paper name
         paper_name = re.sub(r'\W+', ' ', paper_name)
@@ -262,9 +293,7 @@ class GscHtmlFunctions:
         #must get query into the right form as noted by GS link first+middle+last
         query = "+".join(authorFields) + "+" + paper_name
 
-        session = requests.session()
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}
-        response = session.get('https://scholar.google.ca/scholar?q='+query+'&btnG=&hl=en&as_sdt=0%2C5', headers=headers)
+        response = self.session.get('https://scholar-google-ca.proxy.lib.uwaterloo.ca/scholar?q='+query+'&btnG=&hl=en&as_sdt=0%2C5', headers=self.headers)
         soup = BeautifulSoup(response.content, 'lxml')
 
         authorsData = soup.find('div', attrs={'class': 'gs_a'}).findAll('a')
@@ -274,7 +303,7 @@ class GscHtmlFunctions:
             if (anAuthor.text.find(lastName) != -1):
                 link = anAuthor['href']
                 #default number of paper loads and corresponding paper objects stored for author is set to 1
-                thisAuthor = AcademicPublisher('https://scholar.google.ca' + link, 1)
+                thisAuthor = AcademicPublisher('https://scholar-google-ca.proxy.lib.uwaterloo.ca' + link, 1)
                 return thisAuthor
 
         print("cannot find author " + auth_name)
@@ -282,9 +311,16 @@ class GscHtmlFunctions:
 
 
 
-#ozbur = AcademicPublisher('https://scholar.google.ca/citations?user=H1dTZckAAAAJ&hl=en&oi=ao', 8)
+#vas = AcademicPublisher('https://scholar-google-ca.proxy.lib.uwaterloo.ca/citations?user=_yWPQWoAAAAJ', 10)
 
-'''paper1 = Paper('https://scholar.google.ca/citations?view_op=view_citation&hl=en&user=_yWPQWoAAAAJ&citation_for_view=_yWPQWoAAAAJ:u5HHmVD_uO8C')
+#g = GscPdfExtractor()
+#g.findPapersFromCitations('https://scholar-google-ca.proxy.lib.uwaterloo.ca/scholar?start=90&hl=en&as_sdt=0,5&sciodt=0,5&cites=13991517909897415820&scipsc=https://scholar-google-ca.proxy.lib.uwaterloo.ca/scholar?start=90&hl=en&as_sdt=0,5&sciodt=0,5&cites=13991517909897415820&scipsc=')
+
+'''paper1 = Paper('https://scholar-google-ca.proxy.lib.uwaterloo.ca/citations?view_op=view_citation&hl=en&user=_yWPQWoAAAAJ&citation_for_view=_yWPQWoAAAAJ:u5HHmVD_uO8C')
 lst = paper1.findAllAuthors()
 for author in lst:
     print(author.getFirstName() + ' ' + author.getLastName())'''
+
+ref_processor = PaperReferenceExtractor()
+ref_content = ref_processor.getReferencesContent('http://mmlab.snu.ac.kr/~mchen/min_paper/ROCHAS.pdf')
+print(ref_content)
---
+++ offCampusLogin.py
@@ -0,0 +1,17 @@
+import requests
+
+#Internet Setup
+session = requests.Session()
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'} 
+login_page = session.get('https://scholar-google-ca.proxy.lib.uwaterloo.ca/', headers=headers)
+data = {'url':'https://scholar.google.ca/', 'pass': 'jie', 'user':'21187005749502'}
+session.post('https://login.proxy.lib.uwaterloo.ca/login', data)
+
+
+def getHeaders():
+	return headers
+
+def getSesh():
+	return session
+
+
